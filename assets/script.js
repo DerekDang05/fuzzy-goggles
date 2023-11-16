0f3f841a30486fce6948a798b20658980d5a85ac
DerekDang05 <derekdang0801@gmail.com>
2023-11-16 04:55:49 -0500

    testing

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,46 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+// Makes sure DOM is fully loaded before running code
+$(document).ready(function () {
+
+  //function that displays the current day
+  function displayCurrentDay() {
+  const currentDay = dayjs();
+  var currentDayFormat = currentDay.format('dddd, MMMM D');
+  document.getElementById('currentDay').textContent = currentDayFormat;
+  }
+
+  //function that adds specific class depending on the time
+  function addTimeClass() {
+    var currentTime = dayjs().hour();
+    $(".time-block").each(function () {
+      var slotTime = parseInt($(this).attr("id").split("hour")[1] * -1);
+
+      if (slotTime < currentTime) {
+        $(this).addClass("past");
+      } else if (slotTime === currentTime) {
+        $(this).addClass("present")
+      } else if (slotTime > currentTime) {
+        $(this).addClass("future")
+      }
+    })
+  }
+
+  //gets the local storage
+  function getLocalStorage() {
+    for (var i=9; i<=15; i++) {
+        $("#hour-" + i + " textarea").val(localStorage.getItem("hour-" + i));
+    }
+  }
+
+  //click event listener that saves input value to local storage
+  $(".saveBtn").on("click", function () {
+    var slotText = $(this).siblings(".description").val();
+    var slotHour = $(this).parent().attr("id");
+
+    localStorage.setItem(slotHour, slotText);
+
   });
+  //runs all the functions
+    displayCurrentDay();
+    addTimeClass();
+    getLocalStorage();
+})
